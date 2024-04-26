08698c6ea658c5e29577615a4f62923269e8000e
Syed Haider Ali Zaidi <haider.zaidiy@gmail.com>
2024-04-26 12:15:07 +0500

    minor change to view celery logs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from celery.result import AsyncResult
 from fastapi import FastAPI
 from pydantic import BaseModel
 from celery import Celery
@@ -5,11 +6,12 @@ from celery import Celery
 app = FastAPI()
 
 celery = Celery(__name__, broker='redis://localhost:6379/0')
+celery.conf.task_send_sent_event = True
 
 @celery.task
 def long_running_task(a, b):
     import time
-    time.sleep(200)
+    time.sleep(10)
     print("hello")
     return a + b
 
@@ -21,3 +23,8 @@ class TaskRequest(BaseModel):
 def run_task(request: TaskRequest):
     long_running_task.delay(request.a, request.b)
     return {"message": "Task started"}
+
+@app.get("/task_status/{task_id}")
+def get_task_status(task_id: str):
+    task = AsyncResult(task_id)
+    return {"status": task.status, "result": task.result}
